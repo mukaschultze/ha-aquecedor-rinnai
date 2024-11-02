24b133f3136069631ee6c1c0f946c495a753627d
Muka Schultze <samuelschultze@gmail.com>
2024-11-02 00:23:03 +0000

    feat: use `async_forward_entry_setups` for loading platforms

--- custom_components/rinnai_heater/__init__.py
+++ custom_components/rinnai_heater/__init__.py
@@ -36,10 +36,7 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry):
 
         hass.data[DOMAIN][entry.entry_id] = heater
 
-        for component in PLATFORMS:
-            hass.async_create_task(
-                hass.config_entries.async_forward_entry_setup(entry, component)
-            )
+        await hass.config_entries.async_forward_entry_setups(entry, PLATFORMS)
 
         return True
     except ConfigEntryNotReady as ex:
@@ -50,17 +47,8 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry):
             f"Unknown error connecting to device") from ex
 
 
-async def async_unload_entry(hass, entry):
-    """Unload a config entry."""
-    unload_ok = all(
-        await asyncio.gather(
-            *[
-                hass.config_entries.async_forward_entry_unload(
-                    entry, component)
-                for component in PLATFORMS
-            ]
-        )
-    )
+async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry):
+    unload_ok = await hass.config_entries.async_unload_platforms(entry, PLATFORMS)
 
     if not unload_ok:
         return False
