8681054093a30e15075f89b1943efe847d54a40c
Muka Schultze <samuelschultze@gmail.com>
2024-11-22 19:17:52 -0300

    feat: implement target temperature step

--- custom_components/rinnai/water_heater.py
+++ custom_components/rinnai/water_heater.py
@@ -1,9 +1,13 @@
 import logging
-import re
-from typing import Any, Dict, Optional
+from typing import Any
 
-from homeassistant.components.water_heater import WaterHeaterEntity, WaterHeaterEntityFeature, STATE_GAS, STATE_OFF
-from homeassistant.const import UnitOfTemperature, PRECISION_WHOLE
+from homeassistant.components.water_heater import (
+    STATE_GAS,
+    STATE_OFF,
+    WaterHeaterEntity,
+    WaterHeaterEntityFeature,
+)
+from homeassistant.const import PRECISION_WHOLE, UnitOfTemperature
 from homeassistant.core import callback
 
 from .const import DOMAIN, TEMPERATURES_MAP
@@ -34,15 +38,16 @@ class RinnaiHeaterWaterHeater(WaterHeaterEntity):
         self._attr_max_temp = 60
         self._attr_temperature_unit = UnitOfTemperature.CELSIUS
         self._attr_operation_list = [STATE_GAS, STATE_OFF]
-        self._attr_supported_features = WaterHeaterEntityFeature.OPERATION_MODE | WaterHeaterEntityFeature.TARGET_TEMPERATURE
+        self._attr_supported_features = (
+            WaterHeaterEntityFeature.OPERATION_MODE
+            | WaterHeaterEntityFeature.TARGET_TEMPERATURE
+        )
 
     async def async_added_to_hass(self):
-        await self._heater.async_add_rinnai_heater_sensor(
-            self._heater_data_updated)
+        await self._heater.async_add_rinnai_heater_sensor(self._heater_data_updated)
 
     async def async_will_remove_from_hass(self) -> None:
-        await self._heater.async_remove_rinnai_heater_sensor(
-            self._heater_data_updated)
+        await self._heater.async_remove_rinnai_heater_sensor(self._heater_data_updated)
 
     @callback
     def _heater_data_updated(self):
@@ -72,13 +77,16 @@ class RinnaiHeaterWaterHeater(WaterHeaterEntity):
         temperature = kwargs.get("temperature") * 100
 
         nearest_temperature = min(
-            TEMPERATURES_MAP.values(), key=lambda x: abs(x - temperature))
-        nearest_temperature_index = list(
-            TEMPERATURES_MAP.values()).index(nearest_temperature)
+            TEMPERATURES_MAP.values(), key=lambda x: abs(x - temperature)
+        )
+        nearest_temperature_index = list(TEMPERATURES_MAP.values()).index(
+            nearest_temperature
+        )
 
         current_temperature = self.target_temperature * 100
-        current_temperature_index = list(
-            TEMPERATURES_MAP.values()).index(current_temperature)
+        current_temperature_index = list(TEMPERATURES_MAP.values()).index(
+            current_temperature
+        )
 
         steps = nearest_temperature_index - current_temperature_index
 
@@ -106,9 +114,16 @@ class RinnaiHeaterWaterHeater(WaterHeaterEntity):
             await self._heater.lig()
 
     @property
-    def device_info(self) -> Optional[Dict[str, Any]]:
+    def device_info(self) -> dict[str, Any] | None:
         return self._heater._device_info()
 
     @property
-    def available(self) -> Optional[Dict[str, Any]]:
+    def available(self) -> dict[str, Any] | None:
         return True
+
+    @property
+    def capability_attributes(self) -> dict[str, Any]:
+        # https://github.com/home-assistant/core/pull/130722/files
+        data = super().capability_attributes
+        data["target_temp_step"] = PRECISION_WHOLE  # non-standard attribute
+        return data
